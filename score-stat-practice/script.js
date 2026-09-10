56362e31882c5a68c6afeeffbc47780845c8a72f
yeningjie <yeningjie2007@qq.com>
2026-09-10 10:37:37 +0800

    第一次提交：定义课程数组对象，搭建绩点项目基础

---
+++ score-stat-practice/script.js
@@ -0,0 +1,10 @@
+// 课程数据：数组+对象组织，存储课程名称、分数、学分
+const courseList = [
+  {name:"高等数学", score:86, credit:4},
+  {name:"大学英语", score:91, credit:2},
+  {name:"计算机导论", score:-5, credit:3},
+  {name:"体育", score:55, credit:1},
+  {name:"线性代数", score:103, credit:3},
+  {name:"思修", score:76, credit:2}
+];
+console.table(courseList);
